4bed21de8523e837b0e6b1ecbc88723f7e228a15
Evin Darling <hi@evin.io>
2021-10-04 09:43:20 +0100

    Update README.md and add Lab1

---
+++ Lab1/RunnableDemo.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itcarlow.runnabletest;
+
+/**
+ *
+ * @author KEHOEJ
+ */
+class RunnableDemo implements Runnable {
+   private Thread t;
+   private String threadName;
+   
+   RunnableDemo( String name) {
+      threadName = name;
+      System.out.println("Creating " +  threadName );
+   }
+   
+   @Override
+   public void run() {
+      System.out.println("Running " +  threadName );
+      try {
+         for(int i = 4; i > 0; i--) {
+            System.out.println("Thread: " + threadName + ", " + i);
+            // Let the thread sleep for a while.
+            Thread.sleep(50);
+         }
+      } catch (InterruptedException e) {
+         System.out.println("Thread " +  threadName + " interrupted.");
+      }
+      System.out.println("Thread " +  threadName + " exiting.");
+   }
+   
+   public void start () {
+      System.out.println("Starting " +  threadName );
+      if (t == null) {
+         t = new Thread (this, threadName);
+         t.start ();
+      }
+   }
+}
---
+++ Lab1/TestThread.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.itcarlow.runnabletest;
+
+/**
+ *
+ * @author KEHOEJ
+ */
+public class TestThread {
+    public static void main(String args[]) {
+        RunnableDemo R1 = new RunnableDemo( "Thread-1");
+        R1.start();
+        RunnableDemo R2 = new RunnableDemo( "Thread-2");
+        R2.start();
+    }   
+    
+}
